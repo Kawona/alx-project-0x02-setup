110348b63c3dded1a902e4b25e18b9acffe42a7d
Kawona <ojekaona@gmail.com>
2025-09-23 19:26:03 +0000

    Added about.tsx page to pages

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,10 @@
+
+export default function AboutPage() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
+            <h1 className="text-4xl font-bold text-blue-600 mb-4">
+                About Page
+            </h1>
+        </div>
+    );
+}
--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function HomePage() {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
-            <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        <div className="flex min-h-screen flex-col items-center justify-center bg-gray-800">
+            <h1 className="text-4xl font-bold text-gray-50 mb-4">
                 Welcome to my Next.js App
             </h1>
         </div>
